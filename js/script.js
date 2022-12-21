ae9024658bd6e9f0c79899f437bd58942545edb8
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-21 16:36:41 +0100

    prova array oggetti console.log

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log('JS OK');
+
+/* 
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e rendere la pagina gradevole. */
+
+
+
+// Array membri 
+
+const team = [
+    {
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role:  'Founder & CEO',
+
+    }
+]
+
+console.log(team);
+
+
+
